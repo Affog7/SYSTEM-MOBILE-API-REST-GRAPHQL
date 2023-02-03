8122b62d36a97ceb91308f4eca9bdd3c9a7df34c
Augustin <augustin.affognon@etu.uca.fr>
2023-02-03 03:21:44 +0100

    graph ql server

--- Sources/APIQL/Mutations/ProductMutation.cs
+++ Sources/APIQL/Mutations/ProductMutation.cs
@@ -3,6 +3,7 @@ using APIQL.Types;
 using DTOs;
 using GraphQL;
 using GraphQL.Types;
+using NuGet.Protocol.Core.Types;
 using Repository;
 using Services.Abstracts;
 
@@ -22,6 +23,58 @@ namespace APIQL.Mutations
                     var item = context.GetArgument<ProductDTO>("guestInput");
                     return productService.Create(item);
                 });
+
+
+
+            Field<ProductsType>(
+               "updateProduct",
+               arguments: new QueryArguments(
+                   new QueryArgument<NonNullGraphType<ProductInputType>> { Name = "product" },
+                   new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "productId" }),
+               resolve: context =>
+               {
+                   var product = context.GetArgument<ProductDTO>("product");
+                   var productId = context.GetArgument<int>("productId");
+
+                   var dbProduct = productService.GetById(productId);
+                   if (dbProduct == null)
+                   {
+                       context.Errors.Add(new ExecutionError("Couldn't find owner in db."));
+                       return null;
+                   }
+
+                   dbProduct.Name = product.Name;
+                   dbProduct.Price = product.Price;
+                   dbProduct.Quantity = product.Quantity;
+
+                   productService.Update(dbProduct);
+
+                   return dbProduct;
+               }
+           );
+
+
+
+
+            Field<StringGraphType>(
+                "deleteProduct",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "productId" }),
+                resolve: context =>
+                {
+                    var productId = context.GetArgument<int>("productId");
+                    var product = productService.GetById(productId);
+                    if (product == null)
+                    {
+                        context.Errors.Add(new ExecutionError("Couldn't find product in db."));
+                        return null;
+                    }
+
+                    productService.Delete(product);
+
+                    return $"The owner with the id: {productId} has been successfully deleted from db.";
+                }
+            );
+
         }
     }
 }
---
+++ Sources/APIQL/Types/ProductInputType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GraphQL.Types;
+
+namespace APIQL.Types
+{
+	public class ProductInputType : InputObjectGraphType
+	{
+		public ProductInputType()
+		{
+            Name = "productInput";
+            Field<NonNullGraphType<StringGraphType>>("name");
+            Field<NonNullGraphType<FloatGraphType>>("price");
+            Field<NonNullGraphType<IntGraphType>>("quantity");
+        }
+	}
+}
+
