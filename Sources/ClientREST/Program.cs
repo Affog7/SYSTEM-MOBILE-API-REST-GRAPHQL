082fe47958d844ccaef647cd616fa26b486d7036
Augustin <augustin.affognon@etu.uca.fr>
2023-02-12 19:56:01 +0100

    Graph

--- Sources/ClientREST/Program.cs
+++ Sources/ClientREST/Program.cs
@@ -1,3 +1,164 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using DTOs;
+using Newtonsoft.Json;
 
+namespace  ClientREST
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            client.BaseAddress = new Uri("https://localhost:7160/api/v1/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+
+                Console.WriteLine("\n\n-------INFO PRODUCTS---------\n");
+                // Get all products
+
+                Console.WriteLine("\n\n-------LIST PRODUCTS---------\n");
+                var response = client.GetAsync("Products").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string products = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("All products:");
+                    Console.WriteLine(JsonConvert.DeserializeObject(products));
+                }
+                else
+                {
+                    Console.WriteLine("Error while getting all products");
+                }
+
+                Console.WriteLine("\n\n------- PRODUCTS BY ID 2---------\n");
+                // Get product by id
+                int productId = 2;
+                response = client.GetAsync("Products/" + productId).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string product = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("Product with id " + productId + ":");
+                    Console.WriteLine(JsonConvert.DeserializeObject(product));
+                }
+                else
+                {
+                    Console.WriteLine("Error while getting product with id " + productId);
+                }
+
+
+                Console.WriteLine("\n\n-------NEW PRODUCT---------\n");
+                // Insert new product
+                var newProduct = new ProductDTO
+                {
+                    Name = "Product X",
+                    Price = 10,
+                    Quantity = 12
+                };
+
+                response = client.PostAsJsonAsync("Products", newProduct).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("New product inserted");
+                }
+                else
+                {
+                    Console.WriteLine("Error while inserting new product");
+                }
+
+                Console.WriteLine("\n\n-------DELETE PRODUCT PRODUCT---------\n");
+                // Delete product
+                productId = 2;
+                response = client.DeleteAsync("Products/" + productId).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Product with id " + productId + " deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Error while deleting product with id " + productId);
+                }
+
+
+                Console.WriteLine("\n\n---------------------------\n");
+
+                // STOCK
+                Console.WriteLine("\n\n-------INFO STOCKS---------\n");
+                // Get all STOCKS
+
+                Console.WriteLine("\n\n-------ALL STOCKS---------\n");
+                var responseS = client.GetAsync("Stocks").Result;
+                if (responseS.IsSuccessStatusCode)
+                {
+                    string stocks = responseS.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("All stocks:");
+                    Console.WriteLine(JsonConvert.DeserializeObject(stocks));
+                }
+                else
+                {
+                    Console.WriteLine("Error while getting all stocks");
+                }
+
+                Console.WriteLine("\n\n------- STOCKS BY ID ---------\n");
+                // Get stock by id
+                int stocksId = 2;
+                responseS = client.GetAsync("Stocks/" + stocksId).Result;
+                if (responseS.IsSuccessStatusCode)
+                {
+                    string stocks = responseS.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("stocks with id " + stocksId + ":");
+                    Console.WriteLine(JsonConvert.DeserializeObject(stocks));
+                }
+                else
+                {
+                    Console.WriteLine("Error while getting stocks with id " + stocksId);
+                }
+
+
+                Console.WriteLine("\n\n-------NEW STOCKS---------\n");
+                // Insert new stock
+                var newStocks = new StockDTO
+                {
+                    Name = "Product X",
+                };
+
+                responseS = client.PostAsJsonAsync("Stocks", newStocks).Result;
+                if (responseS.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("New  Stocks inserted");
+                }
+                else
+                {
+                    Console.WriteLine("Error while inserting new Stocks");
+                }
+
+                Console.WriteLine("\n\n-------DELETE STOCKS---------\n");
+                // Delete stock
+                stocksId = 2;
+                response = client.DeleteAsync("Stocks/" + stocksId).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Stock with id " + stocksId + " deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Error while deleting Stock with id " + stocksId);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sources/GraphQL.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace GraphQLClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // Set the API endpoint
+                httpClient.BaseAddress = new Uri("https://localhost:5001/graphql");
+                try {
+                    // Build the GraphQL query
+                    var query = "{ products { id, name, price } }";
+                    var content = new StringContent(
+                        "{\"query\":\"" + query + "\"}",
+                        Encoding.UTF8,
+                        "application/json");
+
+
+                    // Send the request to the API
+                    var response =  httpClient.PostAsync("", content).Result;
+
+                    // Read the response
+                    var responseString =  response.Content.ReadAsStringAsync().Result;
+                    var responseJson = JObject.Parse(responseString);
+
+                    // Print the response
+                    Console.WriteLine(responseJson);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Error {e.Message}");
+                }
+                
+            }
+
+
+            
+        }
+    }
+}
